ed1dd0e50e56273d8e4bb95db209703836de0054
Jalal azimi <jalalazimi@gmail.com>
2018-09-22 09:39:19 +0330

    test(tweetFetch): should exports a class

--- test/tweetFetch.test.ts
+++ test/tweetFetch.test.ts
@@ -4,4 +4,8 @@ describe('test', () => {
   it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
+
+  it('should exports a class', function() {
+    expect(typeof TweetFetch).toBe('function')
+  })
 })
